fc38301fb1c93599766877fe457eb174e19dbb17
joelbarmettlerUZH <joel.barmettler@uzh.ch>
2021-05-12 14:54:39 +0200

    include certain node-modules in dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require("path");
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   mode: 'production',
   target: 'node',
+  externals: [nodeExternals({
+    allowlist: ['@actions/core', 'date-fns']
+  })],
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },

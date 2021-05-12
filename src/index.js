9752d9419f431e23b94feb6d1a0f3709d730c2ab
joelbarmettlerUZH <joel.barmettler@uzh.ch>
2021-05-12 14:27:08 +0200

    Make github action async

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import action from "@actions/core";
 import versionAssembler from "./versionassembler.js"
 import pkgjson from "./pkgjson.js";
 
-async function run() {
+function run() {
   const V = action.getInput("V");
   const schema = action.getInput("schema");
   const increasePatch = action.getInput("increase-patch").toLowerCase() === 'true';
 
   const versionString = versionAssembler(schema, V, increasePatch);
-  pkgjson.setVersion(versionString)
+  return pkgjson.setVersion(versionString)
     .catch((error) => {
       action.setFailed(error);
     });

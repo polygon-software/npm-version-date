d7f1efabaaef0f6434567b2ccae3806425240ad7
joelbarmettlerUZH <joel.barmettler@uzh.ch>
2021-05-12 14:35:25 +0200

    self-executing function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import action from "@actions/core";
+import core from "@actions/core";
 import versionAssembler from "./versionassembler.js"
 import pkgjson from "./pkgjson.js";
 
-function run() {
-  const V = action.getInput("V");
-  const schema = action.getInput("schema");
-  const increasePatch = action.getInput("increase-patch").toLowerCase() === 'true';
+(function run() {
+  const V = core.getInput("V");
+  const schema = core.getInput("schema");
+  const increasePatch = core.getInput("increase-patch").toLowerCase() === 'true';
 
   const versionString = versionAssembler(schema, V, increasePatch);
   return pkgjson.setVersion(versionString)
     .catch((error) => {
-      action.setFailed(error);
+      core.setFailed(error);
     });
-}
-
-run();
+})()
